ba442d5848ea69cf50550ff6ccea8c1eda4be515
varshajadhav12345 <virshajadhav565@gmail.com>
2022-11-12 13:31:56 +0530

    FIR-1 create project in spring boot

---
+++ src/main/java/com/BIkkadIT/SpringMvcGitJiraproApplication.java
@@ -0,0 +1,13 @@
+package com.BIkkadIT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcGitJiraproApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMvcGitJiraproApplication.class, args);
+	}
+
+}
